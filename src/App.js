e805956e4f6d2cc603595422599a58914b7abb95
System Administrator <root@Dheerajs-MacBook-Air.local>
2022-02-15 13:37:15 +0530

    added author, time and news source

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
               <Route exact path="/" element={<News key="general" pageSize={5} country="in" category="general"/>} />
               <Route exact path="/business" element={<News key="business" pageSize={5} country="in" category="business"/>} />
               <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment"/>} />
+              <Route exact path="/general" element={<News key="general" pageSize={5} country="in" category="general"/>} />
               <Route exact path="/health" element={<News key="health" pageSize={5} country="in" category="health"/>} />
               <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category="science"/>} />
               <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports"/>} />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,15 +6,17 @@ export class NewsItem extends Component {
 
   render() {
 
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
 
     return (
       <div className="my-3 mx-3">
           <div className="card">
+            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-secondary" style={{left: '90%', zIndex: '1'}}>{source}</span>
             <img src={imageUrl ? imageUrl : "https://i.gadgets360cdn.com/large/reliance_jio_reuters_1608007225500.jpg" } className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-muted">By {author ? author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
                 <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
            </div>
